e22d47af892cde6ce717494330889fd7bcedd01a
Milos Stojanovic <milos.tum@gmail.com>
2021-01-13 21:08:13 +0100

    added miss states

--- main.py
+++ main.py
@@ -16,16 +16,37 @@ s = 0
 incorrect = 5
 guessed_states = []
 text = "Guess the State"
+br = -1
 while incorrect > 0 and len(guessed_states) < 50:
     answer_state = screen.textinput(title=text, prompt="What's another state name?").title()
-
     if answer_state == "Exit":
         missing_states = []
+        x = []
+        y = []
         for state in all_states:
+            br += 1
             if state not in guessed_states:
                 missing_states.append(state)
-        new_data = pandas.DataFrame(missing_states)
+                x_cor = data.x
+                y_cor = data.y
+                x.append(x_cor[br])
+                y.append(y_cor[br])
+        new_dict = {
+            "state": missing_states,
+            "x": x,
+            "y": y
+        }
+        new_data = pandas.DataFrame(new_dict)
         new_data.to_csv("states_to_learn.csv")
+        screen.tracer(0)
+        for i in range(len(missing_states)):
+            exit_t = turtle.Turtle()
+            exit_t.color("red")
+            exit_t.hideturtle()
+            exit_t.penup()
+            exit_t.goto(x[i], y[i])
+            exit_t.write(f"{missing_states[i]}", align="center", font=("Times", 12, "normal"))
+        screen.update()
         break
 
     if (answer_state in all_states) and not(answer_state in guessed_states):
